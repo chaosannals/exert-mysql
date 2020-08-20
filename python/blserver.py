26c4dc1de3430ae4c5ab82f805d94a1514c24810
chaosannals <chaosannals@outlook.com>
2020-08-20 17:01:06 +0800

    记录

--- python/blserver.py
+++ python/blserver.py
@@ -1,4 +1,10 @@
 from pymysqlreplication import BinLogStreamReader
+from pymysqlreplication.row_event import (
+    DeleteRowsEvent,
+    UpdateRowsEvent,
+    WriteRowsEvent,
+)
+from pymysqlreplication.event import RotateEvent
 
 # pymysql 版本，目前只能用 0.9.0 ，pymysqlreplication 不兼容 0.10.0 。github 已经修复，但是作者没有发布到 pypi.org 上。
 mysql_settings = {
@@ -10,10 +16,31 @@ mysql_settings = {
 
 stream = BinLogStreamReader(
     connection_settings=mysql_settings,
-    server_id=100
+    # only_events=[
+    #     DeleteRowsEvent,
+    #     WriteRowsEvent,
+    #     UpdateRowsEvent
+    # ],
+    blocking=False,
+    server_id=2,
+    resume_stream=True,
+    log_file='binlog.000063', # 文件，需要自行记录。RotateEvent 会给出，记得保留。
+    log_pos=4, # 定位号，需要自行记录。每个文件都是从4开始，之后的定位是根据数据长短跳动的
 )
 
 for binlogevent in stream:
-    binlogevent.dump()
+    if isinstance(binlogevent, RotateEvent):
+        print(binlogevent.next_binlog)
+    elif hasattr(binlogevent, 'rows'):
+        for row in binlogevent.rows:
+            if isinstance(binlogevent, DeleteRowsEvent):
+                print(dict(row['values'].items()))
+            elif isinstance(binlogevent, UpdateRowsEvent):
+                print(dict(row['before_values'].items()))
+                print(dict(row['after_values'].items()))
+            elif isinstance(binlogevent, WriteRowsEvent):
+                print(dict(row['values'].items()))
+    else:
+        binlogevent.dump()
 
 stream.close()

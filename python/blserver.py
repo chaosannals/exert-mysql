d3715c7faecb52c26429e70d8b1e6cfab20178f7
chaosannals <chaosannals@outlook.com>
2020-10-27 21:39:13 +0800

    注释

--- python/blserver.py
+++ python/blserver.py
@@ -16,17 +16,30 @@ mysql_settings = {
 
 stream = BinLogStreamReader(
     connection_settings=mysql_settings,
-    # only_events=[
+    # only_events=[ # 只看指定事件
     #     DeleteRowsEvent,
     #     WriteRowsEvent,
     #     UpdateRowsEvent
     # ],
+    # ignored_events=[], # 忽略指定事件
+    # only_tables=['e_tester'], # 只看指定表
+    # ignored_tables=[], # 忽略指定表
+    # only_schemas=[], # 只看指定数据库
+    # ignored_schemas=[], # 忽略指定数据库
     blocking=False,
     server_id=2,
     resume_stream=True, # 必须为 True 时，log_file log_pos 定位才有效。
     log_file='binlog.000099', # 文件，需要自行记录。RotateEvent 会给出，记得保留。
     log_pos=4, # 定位号，需要自行记录。每个文件都是从4开始，之后的定位是根据数据长短跳动的
-    # auto_position='c8671405-081c-11e9-a407-ec0d9a495964:3472692', # 冒号前为 gtid ，冒号后可以用于定序 比如填 1 则接下来得 gtid 冒号后就从 2 开始返回。
+    # GTID 由 source_id:transaction_id 组成
+    # source_id 为发起事务 MySQL 实例 UUID
+    # transaction_id 为事务序号，从 1 开始递增
+    # transaction_id 可以用冒号隔开写多个，减号为连续多个
+    # c8671405-081c-11e9-a407-ec0d9a495964:1-5:11-18
+    # auto_position 会自动过滤掉指定的 gtid 。
+    # auto_position='c8671405-081c-11e9-a407-ec0d9a495964:3472692',
+
+    # skip_to_timestamp='2020-10-01', # 过滤指定时间前的日志
 )
 
 for binlogevent in stream:
